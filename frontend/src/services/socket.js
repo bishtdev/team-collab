8264746ea1c71a302f57d2802a9a4e4d529c9ba8
bishtdev <bishtdev030@gmail.com>
2025-08-10 01:10:28 +0530

    update the socket.js

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,8 +1,20 @@
+// import { io } from 'socket.io-client';
+
+// const socket = io(process.env.NODE_ENV === 'production' 
+//   ? 'https://team-collab-backend-lcge.onrender.com'  // Replace with your actual backend URL
+//   : 'http://localhost:5000', 
+//   {
+//     withCredentials: true,
+//     transports: ['websocket', 'polling']
+//   }
+// );
+
+// export default socket;
+
+
 import { io } from 'socket.io-client';
 
-const socket = io(process.env.NODE_ENV === 'production' 
-  ? 'https://team-collab-backend-lcge.onrender.com'  // Replace with your actual backend URL
-  : 'http://localhost:5000', 
+const socket = io('https://team-collab-backend-lcge.onrender.com', 
   {
     withCredentials: true,
     transports: ['websocket', 'polling']
